4290eddd86ac08085e5a77e48b4096cc9d0465a9
Anton Davydov <dydus0x14@gmail.com>
2015-11-11 22:27:04 +0300

    Added the resources for syntax analyser. Updated .gitignore.

---
+++ res/code3.java
@@ -0,0 +1,33 @@
+package com.github.oxaoo.lexer;
+
+import ssdfsdf;
+import sdsd.sdfvs.sgdsdf;
+
+/**
+ * Created by dydus on 03/11/2015.
+ */
+enum TestEnum {
+    CASE1, CASE2
+};
+
+public class TempClass {
+    public int main(String[] args) {
+        int counter = 3;
+        do {
+            counter++;
+            do {
+                counter++;
+            } while(counter != 10);
+        } while(counter != 10);
+        double a = 19.0;
+        short as = 1;
+        boolean n = true;
+        /*
+            sdvohhdsv
+         */
+        TestEnum st;
+        st = TestEnum.CASE1;
+
+        return counter;
+    }
+}
