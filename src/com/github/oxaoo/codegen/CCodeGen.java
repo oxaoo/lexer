119073ad2d8e0e72d683e729d85a06ed1a6c0544
oxaoo <yol0317@gmail.com>
2015-12-23 23:43:59 +0300

    Getting generation tetrad by the rules

--- src/com/github/oxaoo/codegen/CCodeGen.java
+++ src/com/github/oxaoo/codegen/CCodeGen.java
@@ -41,5 +41,65 @@ public class CCodeGen
 
     }
 
+    private void makeTetrad(int numGr, int numRul, CSyntaxTreeNode node) {
+
+        switch (numGr) {
+            /*case 0:
+                makeTetradWithGr0(numRul, node);
+                break;
+            case 1:
+                makeTetradWithGr1(numRul, node);
+                break;*/
+            case 2:
+                makeTetradWithGr2(numRul, node);
+                break;
+            case 3:
+                makeTetradWithGr3(numRul, node);
+                break;
+            case 4:
+                makeTetradWithGr4(numRul, node);
+                break;
+            case 5:
+                makeTetradWithGr5(numRul, node);
+                break;
+            case 6:
+                makeTetradWithGr6(numRul, node);
+                break;
+        }
+    }
+
+    private void makeTetradWithGr6(int numRul, CSyntaxTreeNode node) {
+
+        //0-1, 3-14.
+    }
+
+    private void makeTetradWithGr5(int numRul, CSyntaxTreeNode node) {
+
+        //0-14.
+    }
+
+    private void makeTetradWithGr4(int numRul, CSyntaxTreeNode node) {
+
+        //0-11.
+    }
+
+    private void makeTetradWithGr3(int numRul, CSyntaxTreeNode node) {
+
+        //0-11.
+    }
+
+    private void makeTetradWithGr2(int numRul, CSyntaxTreeNode node) {
+
+        //2,3,5,7,10,11,13,15.
+    }
+
+    private void makeTetradWithGr1(int numRul, CSyntaxTreeNode node) {
+
+    }
+
+    private void makeTetradWithGr0(int numRul, CSyntaxTreeNode node) {
+
+    }
+
 
 }
--- src/com/github/oxaoo/codegen/CTetrad.java
+++ src/com/github/oxaoo/codegen/CTetrad.java
@@ -5,11 +5,11 @@ public class CTetrad {
     private static int count = 0;
 
     private final EOpcode opcode;
-    private final String operand1;
-    private final String operand2;
+    private final CTetrad operand1;
+    private final CTetrad operand2;
     private final String result;
 
-    public CTetrad(EOpcode opcode, String operand1, String operand2) {
+    public CTetrad(EOpcode opcode, CTetrad operand1, CTetrad operand2) {
 
         this.opcode = opcode;
         this.operand1 = operand1;
@@ -18,11 +18,11 @@ public class CTetrad {
         count++;
     }
 
-    public CTetrad(EOpcode opcode, String operand1, boolean isRes) {
+    public CTetrad(EOpcode opcode, CTetrad operand1, boolean isRes) {
 
         this.opcode = opcode;
         this.operand1 = operand1;
-        this.operand2 = "";
+        this.operand2 = null;
 
         if (isRes) {
             result = String.valueOf(count);
@@ -32,18 +32,32 @@ public class CTetrad {
             result = "";
     }
 
+    // for elem (NO for tetrad).
+    public CTetrad(String str) {
+        opcode = null;
+        operand1 = null;
+        operand2 = null;
+        result = str;
+    }
+
     @Override
-    public boolean equals(Object o)
-    {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
 
         CTetrad cTetrad = (CTetrad) o;
 
-        if (opcode != cTetrad.opcode) return false;
-        if (!operand1.equals(cTetrad.operand1)) return false;
-        if (!operand2.equals(cTetrad.operand2)) return false;
-        return result.equals(cTetrad.result);
+        if (opcode != cTetrad.opcode)
+            return false;
+
+        if (operand1 == null || operand2 == null)
+            return result.equals(cTetrad.result);
+
+        return operand1.equals(cTetrad.operand1)
+                && operand2.equals(cTetrad.operand2)
+                && result.equals(cTetrad.result);
 
     }
 
@@ -51,8 +65,8 @@ public class CTetrad {
     public int hashCode()
     {
         int result1 = opcode.hashCode();
-        result1 = 31 * result1 + operand1.hashCode();
-        result1 = 31 * result1 + operand2.hashCode();
+        //result1 = 31 * result1 + operand1.hashCode();
+        //result1 = 31 * result1 + operand2.hashCode();
         result1 = 31 * result1 + result.hashCode();
         return result1;
     }
@@ -60,10 +74,15 @@ public class CTetrad {
     @Override
     public String toString()
     {
+        String strOp1 = "null";
+        String strOp2 = "null";
+        if (operand1 != null) strOp1 = operand1.toString();
+        if (operand2 != null) strOp2 = operand2.toString();
+
         return "CTetrad{" +
                 "opcode=" + opcode.toString() +
-                ", operand1='" + operand1 + '\'' +
-                ", operand2='" + operand2 + '\'' +
+                ", operand1='" + strOp1 + '\'' +
+                ", operand2='" + strOp2 + '\'' +
                 ", result='" + result + '\'' +
                 '}';
     }
